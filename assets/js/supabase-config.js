c57bb953552c92861515701e4a4675c0fa538c3d
G.Joker👻 <125809602+Mateuzkl@users.noreply.github.com>
2026-07-24 18:15:21 -0300

    chore: Set Supabase URL

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -2,7 +2,7 @@
 // ATENÇÃO: Substitua as variáveis abaixo com os dados reais do seu projeto Supabase.
 // Essas chaves são PÚBLICAS (anon key), não coloque sua service_role ou secret key aqui!
 
-const SUPABASE_URL = 'COLOQUE_A_URL_AQUI'; // <- Você ainda precisa colocar sua URL do Supabase aqui!
+const SUPABASE_URL = 'https://nbmeyglqkuxmqryvoezq.supabase.co';
 const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_blueytj6P4NHsN-9Xrsy7w_2NcKXm-z';
 
 // Initialize Supabase client
